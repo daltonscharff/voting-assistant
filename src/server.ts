c9c783e5bc84409a8e6a9a26cf8b297cbe867845
Dalton Scharff <dalton.scharff@gmail.com>
2020-09-26 15:47:15 -0500

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: express.Application = express();
+
+app.route("/").get((req: express.Request, res: express.Response): void => {
+    res.send({ response: "Ok" });
+});
+
+const port: number = parseInt(process.env.PORT || "8000", 10);
+const host: string = process.env.HOST || "localhost";
+
+app.listen(port, () => console.log(`Server started on http://${host}:${port}`));
